719b5f5475b15ccc1f63f40d0177d165c7473711
mbejtlich <mattbejtlich@gmail.com>
2022-09-22 21:51:51 -0400

    update

--- week 2/grouped bar chart/grouped_barchart.R
+++ week 2/grouped bar chart/grouped_barchart.R
@@ -4,6 +4,8 @@ library(dplyr) # for transforming data
 library(ggplot2) # for plotting data 
 library(ggmap) # for creating maps w/Google Maps
 library(ggsci)
+library(gridExtra)
+library(ggthemes)
 
 # create a dataset
 nyc <- read.csv ("canopystreettree_supp_boro.csv", header=TRUE)
@@ -12,5 +14,5 @@ new_df <- select(nyc,boroname,canopy2010percent,canopy2017percent)
 long <- new_df %>% gather(year, value, -c(boroname))
 
 # Grouped
-ggplot(data, aes(fill=condition, y=value, x=specie)) + 
-  geom_bar(position="dodge", stat="identity")
+ggplot(long, aes(x=boroname, y=value, group=year, fill=year)) + 
+  geom_bar(position="dodge", stat="identity")+scale_y_continuous(expand=c(0,0))+theme_classic()
